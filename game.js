f39ec7d20bc9aa1649d4fda787d539084aab2f72
fleish <fleish@doowop.co>
2018-06-02 16:18:10 -0500

    beginning game control construct

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+let math = require('mathjs')
 let Board = require('./board')
 let Cli = require('./cli')
 
@@ -7,14 +8,44 @@ let Game = {
   state: {
     boards: [],
     winner: null,
-    complete: false,
-    interface: cli
+    interface: cli,
+    status: 'START'
   },
-  handleInput: function(input) {
-    console.log('boom boom boom mr. brown makes thunder:', input)
+  playerMove: function(input) {
+    let target = this.toTarget(input)
+    let board = this.hasStarted() ? this.copyPrior() : this.blankBoard()
+    board.addMove(target)
+    this.state.boards.push(board)
+
+    this.statusCheck(this.aiMove)
   },
+  aiMove: function() {
+
+  },
+  statusCheck: function(callback) {
+     
+    if (callback && !winner) {
+      callback()
+    }
+  },
+  toTarget: function(str) {
+    let index = parseInt(str) - 1
+    let row = Math.trunc(index / 3)
+    let col = index % 3
+    return [row, col]
+  }
+  hasStarted: function() {
+    return this.state.status !== 'START'
+  },
+  copyPrior: function() {
+    let prior = this.boards[this.boards.length]
+    return new Board({ parentKey: prior.key })
+  },
+  blankBoard: function() {
+    return new Board({ parentKey: '0'.repeat(9) })
+  }
   play: function() {
-    cli.start(this.handleInput)
+    cli.start(this.playerMove)
   }
 }
 
