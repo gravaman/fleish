a61bd3b03b2faac8991d4ac782b7551885f4e007
fleish <fleish@doowop.co>
2018-06-05 12:40:29 -0500

    adds css loaders and test css file for app

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,18 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' }
+        ]
+      }
     ]
   }
 }
