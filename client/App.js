5a36bc38d31418e4778d28145de83cc5fe3fb9f2
fleish <fleish@doowop.co>
2018-06-06 11:59:24 -0500

    adds basic tic-tac-toe game structure

--- client/App.js
+++ client/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import TicTacToe from './TicTacToe/Game.js'
 import './App.less'
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
     return(
       <div className="App">
         <h1>Hello World!</h1>
+        <TicTacToe />
       </div>
     )
   }
