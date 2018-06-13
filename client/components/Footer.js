efbfd0196f84da2e46fb38dd3c8bb45f30497d1e
fleish <fleish@doowop.co>
2018-06-13 13:04:54 -0500

    github link opens new tab

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -6,7 +6,7 @@ class Footer extends Component {
     return (
       <footer>
         <a href="mailto:fleish@doowop.co" className="email">fleish@doowop.co</a>
-        <a href="https://github.com/gravaman" className="github">
+        <a href="https://github.com/gravaman" target="_blank" className="github">
           <img src="images/github-mark/PNG/GitHub-Mark-Light-32px.png" />
         </a>
       </footer>
