f848542fa3d6726e3ac713364703435342f37a02
fleish <fleish@doowop.co>
2018-06-15 17:42:24 -0500

    updates for publicPath and mongodb add-on

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,10 @@
 let mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/fleish')
+if (process.env.NODE_ENV !== 'production') {
+  mongoose.connect('mongodb://localhost:27017/fleish')
+} else {
+  mongoose.connect(process.env.MONGODB_URI)
+}
 
 mongoose.connection
   .on('error', () => {})
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -11,6 +11,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
     filename: 'bundle.js'
   },
   module: {
