0c3003c463334af55e07638932c89a0b7ecc77d3
fleish <fleish@doowop.co>
2018-06-27 12:03:02 -0500

    matches yield output to asset frequency

--- server/modules/yielder.js
+++ server/modules/yielder.js
@@ -9,8 +9,7 @@ math.config({
   precision: 64
 })
 
-function bondYld({ rate, cleanPx, redemptionPx = 100, notional = 100, settlement = moment(), exit, frequency = 2, dayCount = 365 }) {
-  // continuous compounding
+function yld({ rate, cleanPx, redemptionPx = 100, notional = 100, settlement = moment(), exit, frequency = 2, dayCount = 365 }) {
   let periods = Periods({ settlement, exit, frequency })
   let cf = CashFlow({ periods, rate, cleanPx, redemptionPx, notional, dayCount })
 
@@ -25,21 +24,8 @@ function bondYld({ rate, cleanPx, redemptionPx = 100, notional = 100, settlement
     x0 = x1
   }
 
-  return x1
-}
-
-function ytm({ ...args }) {
-  // mirrors underlying pmt structure
-  let bondYield = bondYld({ ...args })
-  return toYtm(bondYield)
-}
-
-function toYtm({ m = 2, rc }) {
-  return Calc.compoundM({ m, rc })
-}
-
-function toBondYld({ m = 2, rm }) {
-  return Calc.continuousM({ m, rm })
+  // match compounding rate with frequency
+  return Calc.compoundM({ m: frequency, rc: x1 })
 }
 
 function npv(cf, r) {
@@ -74,46 +60,8 @@ if (require.main === module) {
   let rate = math.bignumber(0.055)
   let cleanPx = math.bignumber(100)
 
-  // let bondYield = Yielder({ rate, cleanPx, exit })
-  // console.log('bond yield:', bondYield.toString())
-  let ytom = ytm({ rate, cleanPx, exit })
-  // let ytm = Calc.equivM({ m: 2, rc: bondYield })
-  console.log('ytm:', ytom.toString())
-
-  // let periods = Periods({ exit })
-  // let pmtDates = periods.dates.slice(1)
-  //
-  // let entry = periods.dates[0]
-  // let m1 = (12 + entry.month() - 6) % 12
-  // let y1 = m1 < entry.month() ? entry.year() : entry.year() - 1
-  // let daysInMonth = moment({ year: y1, month:m1 }).daysInMonth()
-  //
-  // let last = moment({
-  //   year: y1,
-  //   month: m1,
-  //   date: Math.min(exit.date(), daysInMonth)
-  // })
-  //
-  // let accruedPct = entry.diff(last, 'days') / pmtDates[0].diff(last, 'days')
-  //
-  // let annuity = pmtDates.reduce((acc, date, i) => {
-  //   let t = date.diff(moment(), 'days') / 365
-  //   let inc = Math.exp(-bondYield * t)
-  //   if (i === 0) {
-  //     inc *= (1 - accruedPct)
-  //   }
-  //   return acc + inc
-  // }, 0)
-
-  // let t = exit.diff(moment(), 'days') / 365
-  // let terminal = 100 * Math.exp(-bondYield * t)
-  // let parYield = (100 - terminal) * 2 / annuity
-
+  let y1 = yld({ rate, cleanPx, exit })
+  console.log('yield:', y1.toString())
 } else {
-  module.exports = {
-    bondYld,
-    ytm,
-    toYtm,
-    toBondYld
-  }
+  module.exports = { yld }
 }
