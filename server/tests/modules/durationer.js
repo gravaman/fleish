1a5182760e810aa811aef3fbfd0e394158af6ce5
fleish <fleish@doowop.co>
2018-07-07 09:47:52 -0500

    adds modified duration test

--- server/tests/modules/durationer.js
+++ server/tests/modules/durationer.js
@@ -28,7 +28,7 @@ function getPvs(cf, rm, frequency) {
 
 let tests = [
   {
-    msg: 'D === n when r is 0',
+    msg: 'D === 1 when r is 0',
     code: (t) => {
       t.plan(1)
       let cleanPx = 100
@@ -47,7 +47,7 @@ let tests = [
     }
   },
   {
-    msg: 'D < n when r is > 0',
+    msg: 'D === 0.928 when r: 0.1, m:2, cleanPx: 100, maturity: 1yr',
     code: (t) => {
       t.plan(1)
 
@@ -65,37 +65,34 @@ let tests = [
 
       t.equal(result.toString(), expected.toString())
     }
+  },
+  {
+    msg: 'B1 === B0 * (1 - Ddy)',
+    code: (t) => {
+      t.plan(1)
+
+      let cleanPx = 100
+      let r = 0.1
+      let dy = 0.001
+      let r2 = r + dy
+      let m = 2
+      let today = moment()
+      let exit = today.add(365, 'days')
+      let cf = getCf(r, cleanPx, exit)
+      let pvs = getPvs(cf, r, m)
+      let mD = Durationer.duration(cleanPx, r, m, pvs)
+
+      let b0 = pvs.reduce((acc, pv) => Calc.add(acc, pv.amount), 0)
+      let b1 = Calc.multiply(b0, Calc.subtract(1, Calc.multiply(mD, dy)))
+
+      let pvs2 = getPvs(cf, r2, m)
+      let npv2 = pvs2.reduce((acc, pv) => Calc.add(acc, pv.amount), 0)
+
+      let result = math.round(b1, 3)
+      let expected = math.round(npv2, 3)
+      t.equal(result.toString(), expected.toString())
+    }
   }
 ]
-
-// {
-//   msg: 'B1 === B0(1 - Ddy)',
-//   code: (t) => {
-//     t.plan(1)
-//     let dy = 0.001
-//
-//     let cleanPx = 100
-//     let r = 0.1
-//     let today = moment()
-//     let exit = today.add(366, 'days')
-//
-//     let cf = getCf(r, cleanPx, exit)
-//     let d = Durationer.getDuration(cleanPx, cf)
-//     let diff = 1 - Calc.multiply(d, dy)
-//     let result = Calc.multiply(cleanPx, diff)
-//
-//     // get yield
-//     let bondYld = yld({ r, cleanPx, exit })
-//     let rc0 = Calc.continuousM({ m: 2, rm: bondYld })
-//     let rc1 = Calc.add(rc0, dy)
-//
-//     // get npv for updated yield
-//     let fvs = cf.pmts.slice(1)
-//     let dts = fvs.map(fv => fv.date)
-//     let times = dts.map(dt => Calendar.yearsFromToday(dt))
-//     let expected = fvs.reduce((acc, fv, i) => Calc.pv({ fv, r: rc1, t: times[i] }))
-//     t.equal(result.toString(), expected.toString())
-//   }
-// }
 
 tests.forEach(t => test(t.msg, t.code))
