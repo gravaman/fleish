a7118b6899414812f65bb8dccf1af9740e1fb23d
fleish <fleish@doowop.co>
2018-07-18 15:38:41 -0500

    removes portfolio from front end

--- client/App.js
+++ client/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import NavBar from './components/NavBar.js'
 import Footer from './components/Footer.js'
 import TicTacToe from './TicTacToe/Game.js'
-import Portfolio from './FIOptimizer/Portfolio.js'
 import 'font-awesome/css/font-awesome.min.css'
 import './styles.css'
 import './App.css'
@@ -12,7 +11,7 @@ class App extends Component {
     return(
       <div className="app">
         <NavBar />
-        <Portfolio />
+        <TicTacToe />
         <Footer />
       </div>
     )
